d2b0bbe0e47ad7a9a54e7cfdc5f3c6f75350a531
Daniel <schindlerdaniel@web.de>
2022-11-15 12:53:14 +0100

    for + switch

--- script_01.js
+++ script_01.js
@@ -15,4 +15,37 @@
 output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,"," habbe","Sie","die"],"Q"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+
+function getSentence(arr, char){
+    const GAP = " ";
+    zeichen = testChar(char);
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++)
+    {
+        str += arr[i] + GAP;
+    }
+    return str.trim() + zeichen;
+}
+
+function testChar(char) {
+    switch (char) {
+        case "S":
+            return ".";
+        
+        case "Q":
+            return "?";
+
+        case "E":
+            return "!";        
+    
+        default:
+            return "...";
+    }
+}
+
+
+function output(outPutData) {
+    console.log(outPutData);
+}
